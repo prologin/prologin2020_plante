ff5e85b0334cee348d6564599340dd08ce8f188d
pguenezan <paul@guenezan.me>
2020-07-29 01:34:44 +0200

    gui: add dump parsing

--- gui/src/main.js
+++ gui/src/main.js
@@ -12,7 +12,6 @@ let app = new PIXI.Application({
 );
 document.body.appendChild(app.view);
 
-
 function square(size, color) {
   let res = new PIXI.Graphics();
   res.lineStyle(1, 0xFFFFFF, 1);
@@ -25,16 +24,29 @@ function square(size, color) {
 class Cell {
   constructor(str) {
     let split = str.split(",").map(x => parseInt(x));
-    this.a = split[0];
-    this.b = split[1];
-    this.c = split[2];
-    this.sprite = square(TILE_SIZE, 0x008800);
+    this.heat = split[0];
+    this.light = split[1];
+    this.water = split[2];
+    if (this.heat == 0 && this.light == 0 && this.water == 0)
+      var index = 5;
+    else if (this.heat > 0 && this.water == 0)
+      var index = 1;
+    else if (this.heat > 0 && this.water != 0)
+      var index = 2;
+    else if (this.heat == 0 && this.water != 0)
+      var index = 6;
+    else if (this.water != 0)
+      var index = 4;
+    else
+      var index = 3;
+    this.sprite = new PIXI.Sprite(PIXI.loader.resources["sprites/frame" + index + ".png"].texture);
+    this.sprite.width = TILE_SIZE;
+    this.sprite.height = TILE_SIZE;
   }
 }
 
 class Plant {
-  constructor(plant) {
-    console.log('plant', plant);
+  constructor(plant, jardinier) {
     this.pos_x = plant.pos.x;
     this.pos_y = plant.pos.y;
     this.vie_max = plant.vie_max;
@@ -42,7 +54,8 @@ class Plant {
     this.elegance = plant.élégance;
     this.rayon_deplacement = plant.rayon_déplacement;
     this.rayon_collecte = plant.rayon_collecte;
-    this.sprite = new PIXI.Sprite(PIXI.loader.resources["sprites/plant_y.png"].texture);
+    this.jardinier = jardinier;
+    this.sprite = new PIXI.Sprite(PIXI.loader.resources["sprites/plant_" + (this.jardinier == 0 ? "a" : "b") + ".png"].texture);
     this.sprite.height = TILE_SIZE;
     this.sprite.width = TILE_SIZE;
     this.sprite.x = this.pos_x * TILE_SIZE;
@@ -57,6 +70,30 @@ class Map {
     this.p2_plants = [];
     this.sprite = square(TILE_SIZE * 20, 0x000000);
     this.load(dump);
+    this.update_plants(dump);
+  }
+
+  update_plants(dump) {
+    this.p1_plants.forEach(plant => {
+      plant.sprite.visible = false;
+      app.stage.removeChild(plant.sprite);
+    });
+    this.p2_plants.forEach(plant => {
+      plant.sprite.visible = false;
+      app.stage.removeChild(plant.sprite);
+    });
+    this.p1_plants = [];
+    this.p2_plants = [];
+    dump.joueurs[0].plantes.forEach(plant => {
+      let plant_obj = new Plant(plant, 0);
+      this.p1_plants.push(plant_obj);
+      this.sprite.addChild(plant_obj.sprite);
+    });
+    dump.joueurs[1].plantes.forEach(plant => {
+      let plant_obj = new Plant(plant, 1);
+      this.p2_plants.push(plant_obj);
+      this.sprite.addChild(plant_obj.sprite);
+    });
   }
 
   load(dump) {
@@ -74,41 +111,51 @@ class Map {
       }
       this.cells.push(row);
     }
-    dump.joueurs[0].plantes.forEach(plant => {
-      let plant_obj = new Plant(plant);
-      this.p1_plants.push(plant_obj);
-      this.sprite.addChild(plant_obj.sprite);
-    });
-    dump.joueurs[1].plantes.forEach(plant => {
-      let plant_obj = new Plant(plant);
-      this.p2_plants.push(plant_obj);
-      this.sprite.addChild(plant_obj.sprite);
-    });
   }
 }
 
 
-
-
-PIXI.loader.add("dump", "dump.json");
+PIXI.loader.add("dump", "dump.txt");
 PIXI.loader.add("sprites/dog_blue.png");
-PIXI.loader.add("sprites/plant_y.png");
+PIXI.loader.add("sprites/plant_a.png");
+PIXI.loader.add("sprites/plant_b.png");
+for (var i = 1; i <= 6; ++i)
+    PIXI.loader.add("sprites/frame" + i + ".png");
 PIXI.loader.load(setup);
 
+
+let dog, dump, map;
 function setup(loader, resources) {
 
-  let dog = new PIXI.Sprite(PIXI.loader.resources["sprites/dog_blue.png"].texture);
+  dog = new PIXI.Sprite(PIXI.loader.resources["sprites/dog_blue.png"].texture);
   dog.width = 75;
   dog.height = 75;
 
 
-  console.log(resources["dump"].data);
-  let map = new Map(resources["dump"].data);
+  var json_str = "{\"dump\": [" + resources["dump"].data; 
+  json_str = json_str.substring(0, json_str.length - 2) + "]}";
+  dump = JSON.parse(json_str).dump;
+  map = new Map(dump[0]);
   app.stage.addChild(map.sprite);
 
   app.stage.addChild(dog);
 
-  console.log(map);
+  app.ticker.add(delta => gameLoop(delta));
+}
+
+let counter = 0;
+let lastTurn = 0;
+function gameLoop(delta)
+{
+  counter += delta;
+  dog.x += 1;
+  const currentTurn = Math.floor(counter / 60);
+  if (currentTurn != lastTurn)
+  {
+      console.log('currentTurn = ', currentTurn);
+      map.update_plants(dump[currentTurn]);
+  }
+  lastTurn = currentTurn;
 }
 
 
