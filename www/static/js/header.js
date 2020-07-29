28c51cbf3cdf083353a7955f260c8852792f4fa9
pguenezan <paul@guenezan.me>
2020-07-29 18:48:34 +0200

    www: add banner via header.js

---
+++ www/static/js/header.js
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright 2018 Alexandre Macabies
+
+$(function() {
+    $('[role=complementary] h1').text('').html('<object data="/static/img/banner.jpg" style="width:100%; margin-top: 0.5em;" type="image/jpeg" />');
+}
+);
