3bb6dae3e55bdb25dc5f0b49ff1902208612f72e
pguenezan <paul@guenezan.me>
2020-07-29 19:06:12 +0200

    www: add easter egg

--- www/static/js/header.js
+++ www/static/js/header.js
@@ -3,5 +3,27 @@
 
 $(function() {
     $('[role=complementary] h1').text('').html('<object data="/static/img/banner.jpg" style="width:100%; margin-top: 0.5em;" type="image/jpeg" />');
-}
-);
+
+    // let's have some easter egg
+    const p1 = $('<img src="/static/img/rainbow.gif" class="easter-egg" />'),
+        p2 = $('<img src="/static/img/pattern.gif" class="easter-egg" />');
+    $('body').append(p1).append(p2);
+    $('.easter-egg').on('click', function() {
+        const p = $(this);
+        if (p.hasClass('visible')) {
+            p.removeClass('visible');
+        } else {
+            p.removeClass('peak').addClass('visible');
+        }
+    });
+    [p1, p2].forEach(function (p) {
+        if (Math.random() < .5) {
+            p.css('left', 64 + Math.random() * (document.body.clientWidth - 128 - 64));
+            setTimeout(function() {
+                const p = Math.random() < .5 ? p1 : p2;
+                p.addClass('peak');
+            },
+                1000 * (5 + Math.random() * 10));
+        }
+    });
+});
