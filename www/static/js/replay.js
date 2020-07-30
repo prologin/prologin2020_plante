7347bf16ce37b8d7cbebda1d62198acc2110bdfd
pguenezan <paul@guenezan.me>
2020-07-30 09:37:24 +0200

    www: add replay.js

---
+++ www/static/js/replay.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    let $replay = $('#replay'),
+        $playPause = $('#replay-playpause'),
+        $previous = $('#replay-previous'),
+        $next = $('#replay-next'),
+        $turnLabel = $('#replay-turn-label'),
+        $turnSlider = $('#replay-turn-slider');
+
+    $replay.hide();
+
+});
