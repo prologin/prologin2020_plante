641f8aea40d993e8d725210a35452f6d5944408d
Rémi Dupré <r.dupre@qwant.com>
2020-08-01 09:22:12 +0200

    add tests for debug_afficher_chien

---
+++ src/tests/test-action_debug_afficher_drapeau.cc
@@ -0,0 +1,23 @@
+
+#include <gtest/gtest.h>
+
+#include "test-helpers.hh"
+
+TEST_F(ApiTest, test_debug_afficher_chien)
+{
+    for (auto api : p_api)
+    {
+        for (auto color : {AUCUN_CHIEN, CHIEN_BLEU, CHIEN_ROUGE, CHIEN_VERT})
+        {
+            EXPECT_EQ(OK, api->debug_afficher_chien(position{4, 5}, color));
+            EXPECT_EQ(HORS_POTAGER,
+                      api->debug_afficher_chien(position{-1, 5}, color));
+        }
+
+        EXPECT_EQ(CHIEN_INVALIDE,
+                  api->debug_afficher_chien(position{4, 5}, (debug_chien)-1));
+
+        EXPECT_EQ(CHIEN_INVALIDE,
+                  api->debug_afficher_chien(position{4, 5}, (debug_chien)4));
+    }
+}
