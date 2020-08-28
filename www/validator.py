ecbc489b4d24c24163eb17bdb09a5cbbaf941bd7
fnareoh <garance.gourdel@gmail.com>
2020-08-28 16:09:30 +0200

    Fix validator.py

--- www/validator.py
+++ www/validator.py
@@ -13,19 +13,19 @@ def inside_map(row, col):
 
 # Read the grid
 grid = [[[None] * RESSOURCE_COUNT for _ in range(MAP_SIZE)] for __ in range(MAP_SIZE)]
-plants = [[False] * MAP_SIZE] * MAP_SIZE
+plants = [[ False for _ in range(MAP_SIZE)]  for _ in range(MAP_SIZE)]
 
 for row in range(MAP_SIZE):
     line = input()
 
     cells = line.split(' ')
     if len(cells) != MAP_SIZE:
-        exit(f'La grille n\'est pas de la bonne largeur: {len(line)}')
+        exit(f'La grille n\'est pas de la bonne largeur: {len(cells)}')
 
     for col, cell in enumerate(cells):
         res = cell.split(',')
         if len(res) != RESSOURCE_COUNT:
-            exit(f'La case {cell} n\'a pas le bon nombre de ressources')
+            exit(f'La case {cell} n\'a pas le bon nombre de ressources:',len(res))
         for k, r in enumerate(res):
             try:
                 grid[row][col][k] = int(r)
@@ -42,7 +42,7 @@ for player in range(PLAYER_COUNT):
     for _ in range(nb_plants):
         stats = input().split(' ')
         if len(stats) != 7 + RESSOURCE_COUNT:
-            exit(f'La plante {stats} n\'a pas le bon nombre de caractèristiques')
+            exit(f'La plante {stats} n\'a pas le bon nombre de caractèristiques',len(stats))
 
         try:
             row, col, vie, force, pts, mov, grab = list(map(int, stats))[:-RESSOURCE_COUNT]
