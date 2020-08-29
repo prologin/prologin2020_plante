0fd6121351457121c699fe907b26dcdf5ed90397
Rémi Dupré <r.dupre@qwant.com>
2020-08-29 16:45:34 +0200

    add dogo easter egg

--- www/static/js/header.js
+++ www/static/js/header.js
@@ -1,13 +1,38 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // Copyright 2018 Alexandre Macabies
 
+function flip_dogo() {
+    let $elem = $(".easter-egg-42");
+
+    if ($elem.hasClass("right"))
+        $elem.removeClass("right");
+    else
+        $elem.addClass("right");
+}
+
+function plan_42_event(callback) {
+    const wait_for = (102 - (new Date()).getSeconds()) % 60;
+
+    window.setTimeout(
+        () => {
+            callback();
+            plan_42_event(callback);
+        },
+        1000 * wait_for
+    );
+}
+
 $(function() {
     $('[role=complementary] h1').text('').html('<object data="/static/img/banner.jpg" style="width:100%; margin-top: 0.5em;" type="image/jpeg" />');
 
     // let's have some easter egg
     const p1 = $('<img src="/static/img/rainbow.gif" class="easter-egg" />'),
-        p2 = $('<img src="/static/img/pattern.gif" class="easter-egg" />');
-    $('body').append(p1).append(p2);
+        p2 = $('<img src="/static/img/pattern.gif" class="easter-egg" />'),
+        dog = $('<div class="easter-egg-42"><img src="/static/img/dogo.gif" class="dogo" /></div>');
+
+    plan_42_event(flip_dogo);
+
+    $('body').append(p1).append(p2).append(dog);
     $('.easter-egg').on('click', function() {
         const p = $(this);
         if (p.hasClass('visible')) {
@@ -16,6 +41,7 @@ $(function() {
             p.removeClass('peak').addClass('visible');
         }
     });
+
     [p1, p2].forEach(function (p) {
         if (Math.random() < .5) {
             p.css('left', 64 + Math.random() * (document.body.clientWidth - 128 - 64));
