02f1847219ec02de5ec7f880061194bd096cf9d6
Rémi Dupré <r.dupre@qwant.com>
2020-08-30 09:23:18 +0200

    rules: check that a plant still can't hit after moving

--- src/tests/test-action_baffer.cc
+++ src/tests/test-action_baffer.cc
@@ -6,6 +6,7 @@
 #include "test-helpers.hh"
 
 const std::vector<position> player_plant = {{10, 10}, {11, 11}};
+const std::vector<position> player_plant_depot = {{11, 10}, {12, 11}};
 const std::vector<position> player_plant_birth = {{17, 17}, {19, 19}};
 const std::vector<position> player_plant_far = {{16, 10}, {17, 11}};
 const std::vector<position> player_plant_too_far = {{17, 10}, {18, 11}};
@@ -36,6 +37,32 @@ TEST_F(ApiTest, test_action_baffer_ok)
 
     EXPECT_EQ(OK, p_api[0]->baffer(player_plant[0], player_plant[1]));
     EXPECT_EQ(-1, p_api[0]->plante_sur_case(player_plant[1]).vie_max);
+}
+
+TEST_F(ApiTest, test_action_baffer_after_move)
+{
+    for (int player : {0, 1})
+    {
+        EXPECT_EQ(OK, p_api[player]->baffer(player_plant[player],
+                                            player_plant[1 - player]));
+
+        next_turn();
+    }
+
+    for (int player : {0, 1})
+    {
+        EXPECT_EQ(OK, p_api[player]->baffer(player_plant[player],
+                                            player_plant_far[player]));
+
+        EXPECT_EQ(OK, p_api[player]->depoter(player_plant[player],
+                                             player_plant_depot[player]));
+
+        EXPECT_EQ(DEJA_BAFFEE,
+                  p_api[player]->baffer(player_plant_depot[player],
+                                        player_plant_depot[player]));
+
+        next_turn();
+    }
 }
 
 TEST_F(ApiTest, test_action_baffer_err)
